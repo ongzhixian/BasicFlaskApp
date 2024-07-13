e80f5f58e2f9a6c5534393f6c9877150795eb408
ongzhixian <zhixian@hotmail.com>
2024-07-13 11:21:18 +0800

    Add tool

--- webapp/auth.py
+++ webapp/auth.py
@@ -48,16 +48,19 @@ def login():
         user = db.execute(
             'SELECT * FROM user WHERE username = ?', (username,)
         ).fetchone()
+        
+        print("OK1")
 
         if user is None:
             error = 'Incorrect username.'
         elif not check_password_hash(user['password'], password):
             error = 'Incorrect password.'
 
+        print("OK2")
         if error is None:
             session.clear()
             session['user_id'] = user['id']
-            return redirect(url_for('index'))
+            return redirect(url_for('welcome.index'))
 
         flash(error)
 
@@ -67,7 +70,7 @@ def login():
 @bp.route('/logout')
 def logout():
     session.clear()
-    return redirect(url_for('index'))
+    return redirect(url_for('welcome.index'))
 
 @bp.before_app_request
 def load_logged_in_user():
--- webapp/tools.py
+++ webapp/tools.py
@@ -1,3 +1,4 @@
+from logging import getLogger
 from flask import (
     Blueprint, flash, g, redirect, render_template, request, url_for
 )
@@ -8,34 +9,122 @@ from webapp.db import get_db
 
 bp = Blueprint('tools', __name__, url_prefix='/tools')
 
-@bp.route('/')
-def index():
-    return render_template('tools/index.html')
+##############################
+# REPOSITORY
 
-@bp.route('/create', methods=('GET', 'POST'))
+class ToolRepository:
+    """A simple example class"""
+    i = 12345
+    page_size = 6
+    log = getLogger(__name__)
+
+    def get_all_tools(self,page_number):
+        offset = (page_number - 1) * 6
+        print(f"get_all_tools {page_number}")
+        self.log.info(f"LOGINFO get_all_tools {page_number}")
+        db = get_db()
+        tools = db.execute("""
+SELECT 	t.id, t.name, t.description, t.url
+FROM 	tool t 
+ORDER BY t.name ASC
+LIMIT ? OFFSET ?;
+""", (self.page_size, offset)).fetchall()
+        total_record_count = db.execute("""SELECT COUNT(id) count FROM tool""").fetchone()['count']
+        return (tools, total_record_count)
+        
+    def seed(self, item_count=16):
+        tool_description = 'I am a very simple card. I am good at containing small bits of information. I am convenient because I require little markup to use effectively.'
+        user_id = 0
+        db = get_db()
+        for i in range (1, item_count + 1):
+            tool_name = f"Tool {i:03d}"
+            tool_url = f'tool-{i:03d}-url'
+            db.execute(
+                'INSERT INTO tool (name, description, url, user_id)'
+                ' VALUES (?, ?, ?, ?)',
+                (tool_name, tool_description, tool_url, g.user['id'])
+            )
+        db.commit()
+        
+
+##############################
+# ROUTES
+
+tool_repository = ToolRepository()
+
+@bp.route('/')
+@bp.route('/<int:page_number>')
+def index(page_number=1):
+    #tool_repository.seed(16)
+    (tools, total_record_count) = tool_repository.get_all_tools(page_number)
+    #print(f"page_number is {page_number}")
+    return render_template('tools/index.html', tools=tools, 
+        total_record_count=total_record_count,page_number=page_number)
+
+@bp.route('/register', methods=('GET', 'POST'))
 @login_required
-def create():
+def register():
     if request.method == 'POST':
-        title = request.form['title']
-        body = request.form['body']
+        tool_name = request.form['tool_name']
+        tool_description = request.form['tool_description']
+        tool_url = request.form['tool_url']
         error = None
 
-        if not title:
-            error = 'Title is required.'
+        if not tool_name:
+            error = 'Tool name is required.'
 
         if error is not None:
             flash(error)
         else:
             db = get_db()
             db.execute(
-                'INSERT INTO post (title, body, author_id)'
-                ' VALUES (?, ?, ?)',
-                (title, body, g.user['id'])
+                'INSERT INTO tool (name, description, url, user_id)'
+                ' VALUES (?, ?, ?, ?)',
+                (tool_name, tool_description, tool_url, g.user['id'])
             )
             db.commit()
-            return redirect(url_for('blog.index'))
+            return redirect(url_for('tools.index'))
 
-    return render_template('blog/create.html')
+    return render_template('tools/register.html')
+
+
+@bp.route('/edit/<string:id>', methods=('GET', 'POST'))
+@login_required
+def edit(id):
+    print(f"editing tool {id}")
+    if request.method == 'POST':
+        tool_name = request.form['tool_name']
+        tool_description = request.form['tool_description']
+        tool_url = request.form['tool_url']
+        error = None
+
+        if not tool_name:
+            error = 'Tool name is required.'
+
+        if error is not None:
+            flash(error)
+        else:
+            db = get_db()
+            db.execute("""
+                UPDATE tool 
+                SET name = ?
+                    , description = ?
+                    , url = ?
+                    , update_ts = current_timestamp
+                WHERE id = ?;
+                """,
+                (tool_name, tool_description, tool_url, id)
+            )
+            db.commit()
+            return redirect(url_for('tools.index'))
+
+    db = get_db()
+    tool = db.execute(
+        'select t.id, t.name, t.description, t.url from tool t where t.id = ?',
+        (id,)
+    ).fetchone()
+    return render_template('tools/edit.html', tool=tool)
+
 
 def get_post(id, check_author=True):
     post = get_db().execute(
@@ -80,12 +169,4 @@ def update(id):
 
     return render_template('blog/update.html', post=post)
 
-@bp.route('/<int:id>/delete', methods=('POST',))
-@login_required
-def delete(id):
-    get_post(id)
-    db = get_db()
-    db.execute('DELETE FROM post WHERE id = ?', (id,))
-    db.commit()
-    return redirect(url_for('blog.index'))
 
